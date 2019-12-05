108bccf161cd99404205162e28d0ae014053c7b9
BUGkiller <15170799678@163.com>
2019-12-05 11:13:21 +0800

    Update 把数组排成最小的数.cpp

--- 剑指offer(Sword for offer)/把数组排成最小的数.cpp
+++ 剑指offer(Sword for offer)/把数组排成最小的数.cpp
@@ -49,3 +49,22 @@ public:
 	}
 };
 
+// 方法2：定义一个排序规则，进行排序
+class Solution {
+public:
+    string PrintMinNumber(vector<int> numbers) {
+        int len = numbers.size();
+        if(len == 0) return "";
+        sort(numbers.begin(), numbers.end(), cmp);
+        string res;
+        for(int i = 0; i < len; i++){
+            res += to_string(numbers[i]);
+        }
+        return res;
+    }
+    static bool cmp(int a, int b){
+        string A = to_string(a) + to_string(b);
+        string B = to_string(b) + to_string(a);
+        return A < B;
+    }
+};
