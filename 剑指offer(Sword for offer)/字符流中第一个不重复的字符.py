ed88779b4c66409033ebb198a9a6694b9c50f073
BUGkiller <15170799678@163.com>
2019-12-28 11:56:17 +0800

    Create 字符流中第一个不重复的字符.py

---
+++ 剑指offer(Sword for offer)/字符流中第一个不重复的字符.py
@@ -0,0 +1,26 @@
+'''
+// 请实现一个函数用来找出字符流中第一个只出现一次的字符。
+// 例如，当从字符流中只读出前两个字符"go"时，第一个只出现一次的字符是"g"。
+// 当从该字符流中读出前六个字符“google"时，第一个只出现一次的字符是"l"。
+// 如果当前字符流没有存在出现一次的字符，返回#字符。
+'''
+
+class Solution:
+    # 返回对应char
+    def __init__(self):
+        self.s = ''
+        self.dict = {}
+    def FirstAppearingOnce(self):
+        # write code here
+        for i in self.s:
+            if self.dict[i] == 1:
+                return i
+        return '#'
+    def Insert(self, char):
+        # write code here
+        self.s = self.s + char
+        if char in self.dict:
+            self.dict[char] += 1
+        else:
+            self.dict[char] = 1
+            
