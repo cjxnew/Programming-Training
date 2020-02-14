a88d5e406857d96090830260f6aa2f948aafb119
BUGkiller <15170799678@163.com>
2020-02-14 11:11:58 +0800

    Update 二叉搜索树的后序遍历序列.cpp

--- 剑指offer(Sword for offer)/二叉搜索树的后序遍历序列.cpp
+++ 剑指offer(Sword for offer)/二叉搜索树的后序遍历序列.cpp
@@ -14,47 +14,47 @@ using namespace std;
 // 2、遍历序列（除去root结点），找到第一个大于root的位置，则该位置左边为左子树，右边为右子树；
 // 3、遍历右子树，若发现有小于root的值，则直接返回false；
 // 4、分别判断左子树和右子树是否仍是二叉搜索树（即递归步骤1、2、3）。
-//class Solution {
-//public:
-//    bool VerifySquenceOfBST(vector<int> sequence) {
-//        vector<int> leftTree,rightTree;
-//        int root; // 根结点
-//        if(sequence.empty()) return false;
-//        int index = 0; // 标记左右子树界限
-//        int len = sequence.size();
-//        root = sequence[len-1];
-//        int i=0;
-//        for(;i<len-1;++i)
-//        {
-//            if(sequence[i]>root) break; // 找到第一个大于根结点的位置，则左边为左子树，右边为右子树
-//        }
-//        for(int j=i;j<len-1;++j) // 循环时去除root，因此为len-1
-//        {
-//            if(sequence[j]<root) return false; // 有一个小于root，则返回false
-//        }
-//        // 即有左子树
-//        if(i!=0)
-//        {
-//            for(int m=0;m<i;++m)
-//            {
-//                leftTree.push_back(sequence[m]);
-//            }
-//        }
-//		// 即有右子树
-//        if(i!=len-2)
-//        {
-//            for(int j=i;j<len-1;++j)
-//            {
-//                rightTree.push_back(sequence[j]);
-//            }
-//        }
-//         
-//        bool left = true,right = true; // 看左右子树是否是二叉搜索树
-//        if(leftTree.size()>1) VerifySquenceOfBST(leftTree);
-//        if(rightTree.size()>1) VerifySquenceOfBST(rightTree);
-//        return (left&&right);
-//    }
-//};
+class Solution {
+public:
+    bool VerifySquenceOfBST(vector<int> sequence) {
+        vector<int> leftTree,rightTree;
+        int root; // 根结点
+        if(sequence.empty()) return false;
+        int index = 0; // 标记左右子树界限
+        int len = sequence.size();
+        root = sequence[len-1];
+        int i=0;
+        for(;i<len-1;++i)
+        {
+            if(sequence[i]>root) break; // 找到第一个大于根结点的位置，则左边为左子树，右边为右子树
+        }
+        for(int j=i;j<len-1;++j) // 循环时去除root，因此为len-1
+        {
+            if(sequence[j]<root) return false; // 有一个小于root，则返回false
+        }
+        // 即有左子树
+        if(i!=0)
+        {
+            for(int m=0;m<i;++m)
+            {
+                leftTree.push_back(sequence[m]);
+            }
+        }
+		// 即有右子树
+        if(i!=len-2)
+        {
+            for(int j=i;j<len-1;++j)
+            {
+                rightTree.push_back(sequence[j]);
+            }
+        }
+         
+        bool left = true,right = true; // 看左右子树是否是二叉搜索树
+        if(leftTree.size()>1) VerifySquenceOfBST(leftTree);
+        if(rightTree.size()>1) VerifySquenceOfBST(rightTree);
+        return (left&&right);
+    }
+};
 
 // 方法2：非递归。
 //非递归 
